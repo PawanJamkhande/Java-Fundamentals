6d04a59adb3346cb0222548d05042b95107c8a72
Pawan JR. <pawanjamkhande001@gmail.com>
2025-12-01 21:41:10 +0530

    String

---
+++ Arrays/string/ReverseVowels.java
@@ -0,0 +1,34 @@
+package org.string;
+import java.util.*;
+public class ReverseVowels {
+    public static void main(String[] args) {
+        String str = "Reverse a vowels";
+        reverseVowels(str);
+    }
+    public static void reverseVowels(String str){
+        char[] ch = str.toCharArray();
+        int i = 0;
+        int j = ch.length-1;
+        while(i<j){
+            while(ch[i]!='i' && ch[i]!='o' && ch[i]!='u' && ch[i]!='a' && ch[i]!='e'){
+                i++;
+            }
+            while(ch[j]!='i' && ch[j]!='o' && ch[j]!='u' && ch[j]!='a' && ch[i]!='e'){
+                j--;
+            }
+            if(i>=j){
+                break;
+            }
+            char c = ch[i];
+            ch[i] = ch[j];
+            ch[j] = c;
+            i++;
+            j--;
+        }
+        for(char c: ch){
+            System.out.print(c);
+        }
+
+
+    }
+}
