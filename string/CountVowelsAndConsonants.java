94afc9203f666facf52fccabf24e665f9fba20c8
Pawan JR. <pawanjamkhande001@gmail.com>
2025-12-04 00:00:30 +0530

    Add files via upload

---
+++ string/CountVowelsAndConsonants.java
@@ -0,0 +1,22 @@
+package org.string;
+
+import java.util.Scanner;
+
+public class CountVowelsAndConsonants {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = "hello world";
+        int vowels = 0, consonants = 0;
+        str = str.toLowerCase();
+        for(char ch : str.toCharArray()) {
+            if(ch >= 'a' && ch <= 'z') {
+                if("aeiou".indexOf(ch) != -1)
+                    vowels++;
+                else
+                    consonants++;
+            }
+        }
+        System.out.println("Vowels: " + vowels + ", Consonants: " + consonants);
+    }
+}
