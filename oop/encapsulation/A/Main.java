fbcd48eba31627c7b2c2899a1d92afc85afc8d7f
Pawan JR. <pawanjamkhande001@gmail.com>
2025-12-07 23:35:50 +0530

    Add files via upload

---
+++ oop/encapsulation/A/Main.java
@@ -0,0 +1,17 @@
+package org.oop.encapsulation.A;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A();
+
+        for (int i : obj.arr) {
+
+        }
+
+        System.out.println(obj.s);
+
+        String q = obj.s;
+
+
+    }
+}
---
+++ oop/encapsulation/A/Subclass.java
@@ -0,0 +1,10 @@
+package org.oop.encapsulation.A;
+
+public class Subclass {
+
+    public static void main(String[] args) {
+        A ob = new A();
+
+        String n = ob.s;
+    }
+}
