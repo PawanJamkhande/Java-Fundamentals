5c2bb05c128328c9aaa42dbe3608618f2e317ffb
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2025-12-30 23:07:06 +0530

    SearchInSortedArray

---
+++ Arrays/BinarySearch/SearchInSorted.java
@@ -0,0 +1,44 @@
+package BinarySearch;
+import java.util.Scanner;
+
+public class SearchInSorted {
+    public static void main(String[] args){
+        int[] arr= {9,11,14,15,20,22,25,2,3,5,7};
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+        int minidx = findMin(arr);
+        int ans = binarySearch(arr,0, minidx -1, target);
+        if(ans==-1){
+            ans = binarySearch(arr, minidx, arr.length-1, target);
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+    public static int binarySearch(int[] arr, int target, int left, int right){
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(arr[mid]==target){
+                return mid;
+            }else if(arr[mid]<target){
+                left = mid+1;
+            } else{
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static int findMin(int[] arr){
+        int left =0, right = arr.length-1;
+        while(left<right){
+            int mid = (left+right)/2;
+            if(arr[mid]<arr[right]){
+                right = mid;
+            } else{
+                left = mid+1;
+            }
+            }
+            return left;
+
+        }
+    }
+
