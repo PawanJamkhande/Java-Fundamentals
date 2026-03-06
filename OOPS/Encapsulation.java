734602a78eebbdebeb36cfc49af73126d5993579
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-03-06 14:27:16 +0530

    oops encapsulation

---
+++ OOPS/Encapsulation.java
@@ -0,0 +1,32 @@
+package OOPS;
+
+public class Encapsulation {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Encapsulation person = new Encapsulation();
+        person.setName("Alice");
+        person.setAge(30);
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+    
+}
