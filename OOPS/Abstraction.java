2e20e3dd010d72929b923d85eabd2f7e66bee86f
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-03-07 23:41:28 +0530

    new

---
+++ OOPS/Abstraction.java
@@ -0,0 +1,56 @@
+package OOPS;
+/*
+Abstraction means hiding the implementation details and show only essential information of an object 
+example - when you insert the ATM card into the machine you have to enter only required option but you dont see how machine internally working on it
+that hidden working is nothing but abstraction
+To achieve abstraction we are using abstract class and interference 
+Abstract class - it acts as a base class(parent) if we declare class a abstract we cannot create a instance of that class 
+Abstract class contains abstract method as well as non abstract methods 
+To declare method as a absctract class should be abstract 
+At a time you can extend only one abstract class
+Abstract class can contains static data
+ */
+abstract class  A{
+	abstract void ma() ;
+}
+abstract class B extends A{
+	abstract void mb() ;
+}
+
+abstract class C extends B{
+	abstract void mc() ;
+}
+
+class Info extends C{
+
+	@Override
+	void mc() {
+		System.out.println("This is class A method");
+	}
+
+	@Override
+	void mb() {
+		System.out.println("This is class B method");
+		
+	}
+
+	@Override
+	void ma() {
+		System.out.println("This is class C method");
+		
+	}
+	
+}
+
+public class Abstraction {
+
+	public static void main(String[] args) {
+		Info i=new Info();
+		
+		i.ma();
+		i.mb();
+		i.mc();
+
+	}
+
+}
--- OOPS/Encapsulation.java
+++ OOPS/Encapsulation.java
@@ -1,5 +1,8 @@
 package OOPS;
 
+//encapsulation is used for sequrity purpose and to hide the data from outside world and only provide access through getter and setter method
+//In encapsulation we make the data members private and provide public getter and setter methods to access and update the value of private data members
+//give me the code for encapsulation in java to undersantd the concept of encapsulation in java with proper example
 public class Encapsulation {
     private String name;
     private int age;
@@ -24,9 +27,12 @@ public class Encapsulation {
         Encapsulation person = new Encapsulation();
         person.setName("Alice");
         person.setAge(30);
+        
 
         System.out.println("Name: " + person.getName());
         System.out.println("Age: " + person.getAge());
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
     }
-    
+
 }
