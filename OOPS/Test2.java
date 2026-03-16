62843cea7416996edba4cdebe56900ba525155ed
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-03-16 22:13:43 +0530

    test2

---
+++ OOPS/Test2.java
@@ -0,0 +1,32 @@
+package OOPS;
+
+
+ class SBI{
+	void ROI(double roi) {
+		System.out.println("Rate of interest:"+roi+"%");
+	}
+}
+ class BOB extends SBI{
+	 void interest(double roi) {
+			System.out.println("Rate of interest:"+roi+"%");
+		} 
+ }
+ class BOI extends BOB{
+	 void ROILOW(double roi) {
+			System.out.println("Rate of interest:"+roi+"%");
+		}
+ }
+public class Test2 {
+
+	public static void main(String[] args) {
+		/*SBI s1=new SBI();
+		s1.ROI(9.10);
+		BOB b1=new BOB();
+		b1.interest(8.5);*/
+		BOI bi=new BOI();
+		bi.ROI(10.60);
+		bi.interest(8.9);
+		bi.ROILOW(7.5);
+	}
+
+}
