fe1ca9ed681efa4ef4e8c79ca2adc46b8576ef4f
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-03-25 12:25:28 +0530

    Login system

--- Training/Credentials.java
+++ Training/Credentials.java
@@ -7,11 +7,24 @@ public class Credentials {
         String pname = sc.next();
         System.out.println("Enter the password: ");
         String password = sc.next();
-
-        if(pname.equals("rootAdmin")&&password.equals("root")){
-            System.out.println("Valid Credentials");
+        String u_name = "rootAdmin";
+        String pass = "root";
+        int count =0;
+        if(pname.equals(u_name)&&password.equals(pass)&&count<4){
+            System.out.println("Login Successfully!");
+            count++;
         }
-        else System.out.println("Incorrect credentials");
+        else if(pname==u_name && password != pass && count<4){
+            System.out.println("Wrong password! Please try again...");
+            count++;
+        }else if(pname!=u_name && count<4){
+            System.out.println("User not found!");
+            count++;
+        }
+        else if(count>3){
+            System.out.println("Too many wrong attempts. Please contact the office.");
+
+        }
     }
     
 }
