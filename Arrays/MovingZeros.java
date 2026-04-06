7b368e390bcfcf9ed6753b5e714768950a254736
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-04-06 23:35:45 +0530

    moving zeros at the end of the array and the start of the array

---
+++ Arrays/MovingZeros.java
@@ -0,0 +1,50 @@
+public class MovingZeros {
+
+    public static void moveZerosToRight(int[] arr) {
+        int j = 0; // position for non-zero
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+    
+
+
+    public static void moveZerosToLeft(int[] arr) {
+        int j = arr.length - 1; // position for non-zero
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] != 0) {
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+
+        moveZerosToRight(arr);
+        System.out.print("Zero's Shifted to right: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        moveZerosToLeft(arr);
+        System.out.print("Zero's shifted to left: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+
