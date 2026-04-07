f4d84248834607a32d34c9765f5f7af89a99cc91
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-04-07 12:58:49 +0530

    print array matrix

---
+++ Strings/StudentSystem.java
@@ -0,0 +1,10 @@
+package Strings;
+import java.util.*;
+public class StudentSystem {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String students[]={"avdhut","ankita","shalu","pawan","om","sharmeen"};
+
+    }
+}
--- Training/demo.java
+++ Training/demo.java
@@ -1,38 +1,63 @@
 package Training;
 import java.util.*;
 
-class Menu {
-    int MenuId;
-    String MenuName;
-    String MenuType;
-    int price;
-    int qty;
-    Scanner sc = new Scanner(System.in);
+// class Menu {
+//     int MenuId;
+//     String MenuName;
+//     String MenuType;
+//     int price;
+//     int qty;
+//     Scanner sc = new Scanner(System.in);
 
-    void display(){
-        System.out.println("Enter Menu Type: ");
-        MenuType = sc.next();
-        System.out.println("Enter Menu Id: ");
-        MenuId = sc.nextInt();
-        System.out.println("Enter Menu Name: ");
-        MenuName = sc.next();
-        System.out.println("Enter the price: ");
-        price = sc.nextInt();
-        System.out.println("Enter Menu Quantity: ");
-        qty = sc.nextInt();
+//     void display(){
+//         System.out.println("Enter Menu Type: ");
+//         MenuType = sc.next();
+//         System.out.println("Enter Menu Id: ");
+//         MenuId = sc.nextInt();
+//         System.out.println("Enter Menu Name: ");
+//         MenuName = sc.next();
+//         System.out.println("Enter the price: ");
+//         price = sc.nextInt();
+//         System.out.println("Enter Menu Quantity: ");
+//         qty = sc.nextInt();
 
-        System.out.println(MenuId+" "+MenuName+" "+price+" "+qty);
+//         System.out.println(MenuId+" "+MenuName+" "+price+" "+qty);
 
-    }
-    void totalbill(){
-        int totalBill=qty*price;
-        System.out.println("Total Bill is: "+totalBill);
-    }
-}
+//     }
+//     void totalbill(){
+//         int totalBill=qty*price;
+//         System.out.println("Total Bill is: "+totalBill);
+//     }
+// }
+// public class demo{
+//     public static void main(String[] args){
+//         Menu m = new Menu();
+//         m.display();
+//         m.totalbill();
+//     }
+// }
+
 public class demo{
     public static void main(String[] args){
-        Menu m = new Menu();
-        m.display();
-        m.totalbill();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of row: ");
+        int row= sc.nextInt();
+        System.out.println("Enter size of colomn: ");
+        int col = sc.nextInt();
+        int a[][]=new int[row][col];
+        System.out.println("---------Enter Element----------");
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("-------------Print Data-------------");
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
