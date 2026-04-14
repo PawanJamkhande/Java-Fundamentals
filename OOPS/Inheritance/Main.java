7503396d2a1f0f454594c35d92db234cbd4b3e5d
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-04-14 23:42:18 +0530

    Inheritance from another files

--- OOPS/Inheritance/Main.java
+++ OOPS/Inheritance/Main.java
@@ -1,10 +1,30 @@
 package OOPS.Inheritance;
 
 public class Main {
-    public static void main(String[] args){
-        User u = new User();
-        u.login();
-        u.logout();
-        
-    }
+
+	public static void main(String[] args) {
+		
+		Admin a=new Admin();
+		
+		Customer c=new Customer();
+		
+		a.username="Sam Dao";
+		a.login();
+		a.insertProduct("Shoose",2,10000);
+		a.insertProduct("Saree", 10, 15000);
+		a.insertProduct("bag",1, 500);
+		a.showProductDetails();
+		a.logout();
+	
+		
+		c.username="Ankita Sharma";
+		
+		c.login();
+		c.addtoCart(a.p[0]);
+		//c.addtoCart(a.p[1]);
+		c.gcandplaceOrder();
+		c.logout();
+
+	}
+
 }
