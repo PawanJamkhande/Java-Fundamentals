1b14e77cb3f9812671806efbbc9f4df7091c069e
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-04-15 22:29:38 +0530

    print N after decimal

---
+++ Strings/PrintNFloat.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class PrintNFloat {
+    public static void main(String[] args){
+        double num = 27.992638970;
+        System.out.println("4 no. after decimal: "+ String.format("%.4f",num));
+    }
+    
+}
