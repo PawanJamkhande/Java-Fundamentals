4f040e8b668f473cd47023ac57dcc8a5bad22659
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-06-16 12:57:05 +0530

    lambda expression

--- Java8/Funtional.java
+++ Java8/Funtional.java
@@ -5,16 +5,35 @@ package Java8;
 //code when working with functional programming concepts.  
 //funtional interface meaning SAM (Single Abstract Method) interface is an interface that contains only one abstract method. It can have multiple default and static methods, but it must have exactly one abstract method. Functional interfaces are used as the basis for lambda expressions and method references in Java 8 and later versions. They provide a target type for lambda expressions, allowing you to write more concise and readable code when working with functional programming concepts.
 //The @FunctionalInterface annotation is used to indicate that an interface is intended to be a functional interface. It is not mandatory to use this annotation, but it helps to ensure that the interface meets the requirements of a functional interface and provides better readability for developers.
+
 @FunctionalInterface
-interface MyFunctionalInterface {
-    void myMethod(); // Abstract method
+interface Div{
+    void div(int a,int b);
+}
+interface Add{
+    void add(int a,int b);
+}
+interface Sub{
+    void sub(int a,int b);
+}
+interface Mul{
+    void mul(int a,int b);
 }
 public class Funtional {
     public static void main(String[] args) {
         // Using a lambda expression to implement the functional interface
-        MyFunctionalInterface myFunc = () -> System.out.println("Hello, Functional Interface!");
-        
+        Div d=(a,b)-> System.out.println("Division is "+ (a/b));
+        Add a=(x,y)-> System.out.println("Addition is "+ (x+y));
+        Sub s=(x,y)->System.out.println("Subtraction is "+ (x-y));
+         Mul m=(x,y)->System.out.println("Multiplication is "+ (x*y));
         // Calling the method defined in the functional interface
-        myFunc.myMethod();
+
+        d.div(10, 2);
+        a.add(10,30);
+        
+        s.sub(30,10);
+       
+        m.mul(10,2);
+
     }
 }
