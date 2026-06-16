d9272de53e92b415098a77a01c16741c2fcc6bf8
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-06-16 14:51:36 +0530

    Predicate

---
+++ Java8/Types/Predicate1.java
@@ -0,0 +1,19 @@
+package Java8.Types;
+import java.util.function.Predicate;
+/*
+3. Predicate : It represents a boolean-valued function of one argument. It is often used for filtering or matching operations. 
+              boolean test(T t);
+              T is the type of the input to the operation.
+              t is the input argument to the operation.
+              Does not need to declare interface because it is a functional interface and it is already defined in java.util.function package.
+ */
+public class Predicate1 {
+    public static void main(String[] args){
+        Predicate<Integer> even = (n) -> n % 2 == 0;
+        System.out.println(even.test(4)); // Output: true
+        System.out.println(even.test(5)); // Output: false
+        Predicate<String> startsWithA = (s) -> s.startsWith("A");
+        System.out.println(startsWithA.test("Apple")); // Output: true
+        System.out.println(startsWithA.test("Banana")); // Output: false
+    }
+}
