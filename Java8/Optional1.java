f4c82e84a4667fd372f0cdfa96dd778cde41442b
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-06-17 15:16:48 +0530

    optional

---
+++ Java8/Optional1.java
@@ -0,0 +1,34 @@
+/*
+OPtional is an container introduced in java 8 used to handle NullPointerException
+instead of returning null, method returns optional object that may contain value or may not contain value.
+
+isPresent() - returns true if values present otherwise false
+isEmpty() - returns true if there is no value
+get() - returns value if present otherwise throws NoSuchElementexpection
+orElse(T other) - returns the value if present otherwise returns the default value
+of(t value) - create optional containing a non null values. is value is null throws NullPointerException
+ofNullable(T value) - create optional may contains value or may be empty or may be null
+*/
+package Java8;
+
+import java.util.Optional;
+
+public class Optional1{
+    public static void main(String[] args){
+        Optional<String> o1 = Optional.of("Hello, World!");
+        
+        System.out.println(o1.get());
+
+        Optional<String> o2 = Optional.empty();
+        System.out.println(o2.isEmpty());
+
+        Optional<String> o3 = Optional.ofNullable(null);
+        System.out.println(o3.isEmpty());
+
+        // Optional<String> o4 = Optional.of(null);
+        // System.out.println(o4.isEmpty());
+
+        String s = o3.orElse("Default Value");
+        System.out.println(s);
+    }
+}
