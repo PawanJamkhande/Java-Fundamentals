2f2029c299525f9280fdb1150d5211b83dfcc673
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-06-23 23:43:47 +0530

    js event

--- WebDevelopment/InterviewQuestions/Prototype.js
+++ WebDevelopment/InterviewQuestions/Prototype.js
@@ -11,3 +11,39 @@ Post.prototype.disp=function(){
 let p=new Post("Java is secure")
 
 p.disp()
+
+class A{
+    constructor(name){
+  this.name=name;
+    }
+
+    show(){
+        console.log(this.name)
+    }
+}
+
+let a=new A("Maya")
+
+a.show();
+
+
+
+
+
+
+
+
+function Post(pname){
+    this.pname=pname;
+}
+
+// add method prototype
+
+Post.prototype.disp=
+function(){
+    console.log("Post Name:"+this.pname)
+};
+
+let l=new Post("Java is secure")
+
+l.disp()
