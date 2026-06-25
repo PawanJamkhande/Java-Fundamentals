d0c1bd8dece84353bac411ef764f007cdc28408e
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-06-25 14:33:28 +0530

    functions in ts

--- WebDevelopment/TypeScript/demo.js
+++ WebDevelopment/TypeScript/demo.js
@@ -47,3 +47,35 @@ emp.push("Raman");
 console.log(emp);
 emp.pop();
 console.log(emp);
+let p1 = {
+    postId: 101,
+    postName: "Java is secure",
+    postpublished: true
+};
+console.log(p1);
+function disp() {
+    console.log("This is Typescript function");
+}
+disp();
+function add(a, b) {
+    return (a + b);
+}
+console.log(add(10, 5));
+//optional function
+function greet(name, age, weight) {
+    console.log(name + " " + age + " " + weight);
+}
+greet("sam", 24);
+// const num= (x:number):
+// number =>{
+//     return x*x;
+// }
+// console.log(10)
+const info = (name) => {
+    return "Name:" + name;
+};
+console.log(info("sam Dao"));
+let result = (i) => {
+    return i;
+};
+console.log(result(100));
--- WebDevelopment/TypeScript/demo.ts
+++ WebDevelopment/TypeScript/demo.ts
@@ -57,3 +57,58 @@ console.log(emp)
 
 emp.pop();
 console.log(emp)
+
+
+//Interface In typescript
+
+interface Post{
+    postId:number;
+    postName:string;
+    postpublished:boolean;
+}
+
+let p1:Post={
+    postId :101,
+    postName:"Java is secure",
+postpublished:true
+
+
+}
+console.log(p1)
+
+function disp():void{
+    console.log("This is Typescript function")
+}
+disp();
+
+function add(a :number,b:number):number{
+    return (a+b);
+}
+console.log(add(10,5))
+//optional function
+
+function greet(name:string,age:number,weight?:number):void{
+    console.log(name+" "+age+" "+weight)
+} 
+
+greet("sam",24);
+
+// const num= (x:number):
+// number =>{
+//     return x*x;
+// }
+// console.log(10)
+
+const info=(name:string):
+string =>{
+    return "Name:"+name;
+}
+
+
+console.log(info("sam Dao"))
+
+let result=(i:number):
+number =>{
+    return i;
+}
+console.log(result(100));
