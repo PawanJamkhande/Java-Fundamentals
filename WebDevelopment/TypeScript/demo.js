9adc9c43cc023497b07a8246247540f2bcb1dbd8
PawanJamkhande <pawan.jamkhande23@comp.sce.edu.in>
2026-06-25 11:54:05 +0530

    typescript demo

---
+++ WebDevelopment/TypeScript/demo.js
@@ -0,0 +1,26 @@
+"use strict";
+/*
+TypeScript :
+Datatype
+1) Any Type - it is dynamic type that bypass data types
+2) Built in types - boolean , null, number, string, undefined , void
+3)
+*/
+// let x:number=10;
+// let y:number=35;
+// console.log(x+y)
+// let m:string="Hello, Welcome to the TypeScript";
+// console.log(m)
+let isTrue = true;
+let emptyValue = null;
+let num = 109;
+let str = "Hello bachhooonn!!";
+let uninitializedvar = undefined;
+function logMessage() {
+    console.log("This function doesn't return anythin.");
+}
+console.log(isTrue);
+console.log(emptyValue);
+console.log(num);
+console.log(str);
+console.log(uninitializedvar);
---
+++ WebDevelopment/TypeScript/demo.ts
@@ -0,0 +1,30 @@
+/*
+TypeScript : 
+Datatype 
+1) Any Type - it is dynamic type that bypass data types 
+2) Built in types - boolean , null, number, string, undefined , void
+3) 
+*/
+
+// let x:number=10;
+// let y:number=35;
+// console.log(x+y)
+
+// let m:string="Hello, Welcome to the TypeScript";
+// console.log(m)
+
+let isTrue:boolean=true;
+let emptyValue:null=null;
+let num:number=109;
+let str:string="Hello bachhooonn!!";
+let uninitializedvar:undefined=undefined;
+
+function logMessage():void{
+    console.log("This function doesn't return anythin.");
+}
+
+console.log(isTrue);
+console.log(emptyValue);
+console.log(num);
+console.log(str);
+console.log(uninitializedvar);
